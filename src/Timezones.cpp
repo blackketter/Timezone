c003d3fdcc3b91ec06bef5cf2c2dda0092f4c7a8
Dean Blackketter <dean@blackketter.com>
2018-09-22 20:57:35 -0700

    Timezone examples can now be included more than once

---
+++ src/Timezones.cpp
@@ -0,0 +1,26 @@
+#include "Timezones.h"
+
+TimeChangeRule usPDT = {"PDT", Second, Sun, Mar, 2, -420};
+TimeChangeRule usPST = {"PST", First, Sun, Nov, 2, -480};
+TimeChangeRule usMDT = {"MDT", Second, Sun, Mar, 2, -360};
+TimeChangeRule usMST = {"MST", First, Sun, Nov, 2, -420};
+TimeChangeRule usCDT = {"CDT", Second, Sun, Mar, 2, -300};
+TimeChangeRule usCST = {"CST", First, Sun, Nov, 2, -360};
+TimeChangeRule usEDT = {"EDT", Second, Sun, Mar, 2, -240};  //Eastern Daylight Time = UTC - 4 hours
+TimeChangeRule usEST = {"EST", First, Sun, Nov, 2, -300};   //Eastern Standard Time = UTC - 5 hours
+TimeChangeRule aEDT = {"AEDT", First, Sun, Oct, 2, 660};    //UTC + 11 hours
+TimeChangeRule aEST = {"AEST", First, Sun, Apr, 3, 600};    //UTC + 10 hours
+TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     //Central European Summer Time
+TimeChangeRule CET = {"CET ", Last, Sun, Oct, 3, 60};       //Central European Standard Time
+TimeChangeRule BST = {"BST", Last, Sun, Mar, 1, 60};        //British Summer Time
+TimeChangeRule GMT = {"GMT", Last, Sun, Oct, 2, 0};         //Standard Time
+
+Timezone CE(CEST, CET);
+Timezone ausET(aEDT, aEST);
+Timezone UK(BST, GMT);
+Timezone usET(usEDT, usEST);
+Timezone usCT(usCDT, usCST);
+Timezone usMT(usMDT, usMST);
+Timezone usAZ(usMST, usMST);
+Timezone usPT(usPDT, usPST);
+Timezone UTC(GMT,GMT);
--- src/Timezones.h
+++ src/Timezones.h
@@ -3,42 +3,31 @@
 
 #include "Timezone.h"
 
+// UTC time
+extern Timezone UTC;
+
 //Australia Eastern Time Zone (Sydney, Melbourne)
-TimeChangeRule aEDT = {"AEDT", First, Sun, Oct, 2, 660};    //UTC + 11 hours
-TimeChangeRule aEST = {"AEST", First, Sun, Apr, 3, 600};    //UTC + 10 hours
-Timezone ausET(aEDT, aEST);
+extern Timezone ausET;
 
 //Central European Time (Frankfurt, Paris)
-TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     //Central European Summer Time
-TimeChangeRule CET = {"CET ", Last, Sun, Oct, 3, 60};       //Central European Standard Time
-Timezone CE(CEST, CET);
+extern Timezone CE;
 
 //United Kingdom (London, Belfast)
-TimeChangeRule BST = {"BST", Last, Sun, Mar, 1, 60};        //British Summer Time
-TimeChangeRule GMT = {"GMT", Last, Sun, Oct, 2, 0};         //Standard Time
-Timezone UK(BST, GMT);
+extern Timezone UK;
 
 //US Eastern Time Zone (New York, Detroit)
-TimeChangeRule usEDT = {"EDT", Second, Sun, Mar, 2, -240};  //Eastern Daylight Time = UTC - 4 hours
-TimeChangeRule usEST = {"EST", First, Sun, Nov, 2, -300};   //Eastern Standard Time = UTC - 5 hours
-Timezone usET(usEDT, usEST);
+extern Timezone usET;
 
 //US Central Time Zone (Chicago, Houston)
-TimeChangeRule usCDT = {"CDT", Second, Sun, Mar, 2, -300};
-TimeChangeRule usCST = {"CST", First, Sun, Nov, 2, -360};
-Timezone usCT(usCDT, usCST);
+extern Timezone usCT;
 
 //US Mountain Time Zone (Denver, Salt Lake City)
-TimeChangeRule usMDT = {"MDT", Second, Sun, Mar, 2, -360};
-TimeChangeRule usMST = {"MST", First, Sun, Nov, 2, -420};
-Timezone usMT(usMDT, usMST);
+extern Timezone usMT;
 
 //Arizona is US Mountain Time Zone but does not use DST
-Timezone usAZ(usMST, usMST);
+extern Timezone usAZ;
 
 //US Pacific Time Zone (Las Vegas, Los Angeles)
-TimeChangeRule usPDT = {"PDT", Second, Sun, Mar, 2, -420};
-TimeChangeRule usPST = {"PST", First, Sun, Nov, 2, -480};
-Timezone usPT(usPDT, usPST);
+extern Timezone usPT;
 
 #endif
